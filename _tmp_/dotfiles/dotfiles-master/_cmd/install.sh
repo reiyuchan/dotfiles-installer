ca8a34f00b50a18007d0404d26822c0334e065a1
reiyuchan <iwakura.rei.tk@gmail.com>
2024-05-10 08:26:24 +0300

    removed _tmp_ directory

--- _tmp_/dotfiles/dotfiles-master/_cmd/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Copyright (c) 2023 reiyuchan <iwakura.rei.tk@gmail.com>
-# Released under the MIT license
-# http://opensource.org/licenses/mit-license.php
-
-set -eux
-
-echo Error:This can only be installed for windows.

c9d5a37ffd6eebba506e60719d434cc1f04a313b
reiyuchan <iwakura.rei.tk@gmail.com>
2024-05-11 20:10:25 +0300

    refactor move download func to separate module

--- src/main.go
+++ src/main.go
@@ -2,44 +2,12 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"runtime"
 
-	"github.com/reiyuchan/dotfiles-installer/controllers"
-	"github.com/reiyuchan/dotfiles-installer/execute"
+	"github.com/reiyuchan/dotfiles-installer/download"
 )
 
-func WhichOS() string {
-	return runtime.GOOS
-}
-
-func Download() (err error) {
-	// const win_url = "https://raw.githubusercontent.com/reiyuchan/dotfiles/master/quick-installer/install.bat"
-	// const unix_url = "https://raw.githubusercontent.com/reiyuchan/dotfiles/master/quick-installer/install.sh"
-	const url = "https://github.com/reiyuchan/dotfiles/archive/refs/heads/master.zip"
-	path := os.Getenv("HOME")
-	switch currentOS := WhichOS(); currentOS {
-	case "windows":
-		path += "\\dotfiles"
-		err := controllers.GetFile(url, path)
-		execute.Execute(path, currentOS)
-		if err != nil {
-			return fmt.Errorf("Error:" + err.Error())
-		}
-
-	default:
-		path += "/dotfiles"
-		err := controllers.GetFile(url, path)
-		execute.Execute(path, currentOS)
-		if err != nil {
-			return fmt.Errorf("Error:" + err.Error())
-		}
-	}
-	return nil
-}
-
 func main() {
-	err := Download()
+	err := download.Download()
 	if err != nil {
 		fmt.Println(err.Error())
 	} else {
